a0531afdd2d1568e05e8a826347647531f51e9b2
krupasagar540 <119719128+krupasagar540@users.noreply.github.com>
2022-12-10 10:13:04 +0530

    #include<stdio.h> int main() {     int N;     scanf("%d",&N);     int a[N],i,sum=0;     for(i=0;i<N;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<N;i++)     {         if(a[i]%2!=0)         {             sum=sum+a[i];         }     }      printf("%d",sum); }

---
+++ ODD_Sum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    int a[N],i,sum=0;
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<N;i++)
+    {
+        if(a[i]%2!=0)
+        {
+            sum=sum+a[i];
+        }
+    }
+     printf("%d",sum);
+}
