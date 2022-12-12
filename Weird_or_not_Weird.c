033243c7cb57a450857a4bcc6bdd3749c42957f9
krupasagar540 <119719128+krupasagar540@users.noreply.github.com>
2022-12-12 06:55:19 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if(n%2!=0)     {         printf("weird");     }     else if(n>=2&&n<=5)     {         printf("not weird");     }     else if(n>=6&&n<=20)     {         printf("weird");     }     else     {         printf("not weird");     } }

---
+++ Weird_or_not_Weird.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%2!=0)
+    {
+        printf("weird");
+    }
+    else if(n>=2&&n<=5)
+    {
+        printf("not weird");
+    }
+    else if(n>=6&&n<=20)
+    {
+        printf("weird");
+    }
+    else
+    {
+        printf("not weird");
+    }
+}
