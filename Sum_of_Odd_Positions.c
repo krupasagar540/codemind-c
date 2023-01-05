a445b66bbe8d8e47c121cc1a664fa83151991b67
krupasagar540 <119719128+krupasagar540@users.noreply.github.com>
2023-01-05 09:58:03 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,s=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(i%2!=0)         {             s=s+a[i];         }     }     printf("%d",s); }

--- Sum_of_Odd_Positions.c
+++ Sum_of_Odd_Positions.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,sum=0;
+    int n;
     scanf("%d",&n);
-    int a[n];
+    int a[n],i,s=0;
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
@@ -12,9 +12,8 @@ int main()
     {
         if(i%2!=0)
         {
-            sum=sum+a[i];
+            s=s+a[i];
         }
-        
     }
-    printf("%d",sum);
+    printf("%d",s);
 }
