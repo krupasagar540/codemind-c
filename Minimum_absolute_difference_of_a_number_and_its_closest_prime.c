36d18731f17c127beac2af33c7c8f3b62110c1b5
krupasagar540 <119719128+krupasagar540@users.noreply.github.com>
2023-01-14 10:36:14 +0530

    #include<stdio.h> int main() {     int n,i,j,k,np,sp,c=0,d,e;     scanf("%d",&n);     for(j=n;j>=n-10;j--)     {         c=0;         for(k=1;k<=j;k++)         {             if(j%k==0)             {                 c++;             }         }         if(c==2)         {             np=j;             d=n-np;             break;         }     }     for(j=n;j<=n+10;j++)     {         c=0;         for(k=1;k<=j;k++)         {             if(j%k==0)             {                 c++;             }         }         if(c==2)         {             sp=j;             e=sp-n;             break;         }     }     if(d<e||d==e)     {         printf("%d",d);     }         else         {             printf("%d",e);         } }

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,k,np,sp,c=0,d,e;
+    scanf("%d",&n);
+    for(j=n;j>=n-10;j--)
+    {
+        c=0;
+        for(k=1;k<=j;k++)
+        {
+            if(j%k==0)
+            {
+                c++;
+            }
+        }
+        if(c==2)
+        {
+            np=j;
+            d=n-np;
+            break;
+        }
+    }
+    for(j=n;j<=n+10;j++)
+    {
+        c=0;
+        for(k=1;k<=j;k++)
+        {
+            if(j%k==0)
+            {
+                c++;
+            }
+        }
+        if(c==2)
+        {
+            sp=j;
+            e=sp-n;
+            break;
+        }
+    }
+    if(d<e||d==e)
+    {
+        printf("%d",d);
+    }
+        else
+        {
+            printf("%d",e);
+        }
+}
